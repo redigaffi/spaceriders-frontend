df4b1d53b345d266ca280a4d2127dc1eeb1382d9
Muhammad Azhar <48376727+muazhar@users.noreply.github.com>
2021-10-01 18:07:21 +0500

    Defense

--- src/pages/Defense.vue
+++ src/pages/Defense.vue
@@ -18,50 +18,109 @@
               ></DefaultSlider>
             </div>
           </q-slide-transition>
-          <q-slide-transition duration=1000>
+          <q-slide-transition duration="1000">
             <div v-show="currentTabComponent != 'DefaultSlider'">
-              <InfoSlider
-                key="researchSlider"
-                :data="researchInfo"
-                type="installation"
-                @cancelled="cancelled"
-              />
+              <DefenseSlider key="defenseSlider" @cancelled="cancelled" />
             </div>
           </q-slide-transition>
         </div>
       </q-card>
     </div>
 
-    <ItemList
-      listName="Research"
-      :data="this.$store.getters.researchData"
-      @change="slideDiv"
-    />
+    <div class="q-pt-sm">
+      <q-card class="glass-element text-white q-pa-md">
+        <q-card-section class="row fit justify-between items-center">
+          <div
+            class="
+              text-h6 text-center text-weight-bold text-secondary
+              absolute-center
+            "
+          >
+            LIST OF ITEMS
+          </div>
+        </q-card-section>
+        <div>
+          <q-card-section class="row q-col-gutter-sm justify-start">
+            <div
+              class="col-xs-6 col-sm-4 col-md-2 q-pa-sm text-center"
+              v-for="(value, key) in 8"
+              :key="key"
+            >
+              <q-card
+                flat
+                class="bg-transparent text-dark cursor-pointer"
+                @click="slideDiv"
+              >
+                <q-btn
+                  dense
+                  size="md"
+                  class="absolute-top"
+                  color="warning"
+                  icon="expand_less"
+                  style="z-index: 1"
+                >
+                  <q-tooltip
+                    anchor="top middle"
+                    self="center middle"
+                    class="bg-primary text-subtitle2"
+                  >
+                    Upgrade PROD to level 2
+                  </q-tooltip>
+                </q-btn>
+                <q-card-section class="q-pa-none">
+                  <q-card flat class="bg-transparent text-dark cursor-pointer">
+                    <img src="~assets/img/planet1.jpg" style="height: 110px" />
+
+                    <q-card-section
+                      class="
+                        q-pa-xs
+                        text-warning
+                        absolute-bottom
+                        tag-glass-element
+                      "
+                    >
+                      2
+                    </q-card-section>
+                    <!-- <q-card-section class="bg-dark">
+                    <q-btn color="warning" class="q-px-lg" label="Claim" />
+                  </q-card-section> -->
+                    <q-tooltip
+                      anchor="top middle"
+                      self="center middle"
+                      class="bg-primary text-subtitle2"
+                    >
+                      PROD
+                    </q-tooltip>
+                  </q-card>
+                </q-card-section>
+              </q-card>
+            </div>
+          </q-card-section>
+        </div>
+      </q-card>
+    </div>
   </div>
 </template>
 
 <script>
 import { defineComponent, ref, computed } from "vue";
-import InfoSlider from "src/components/lvl_up/InfoSlider.vue";
+import DefenseSlider from "src/components/DefenseSlider.vue";
 import DefaultSlider from "src/components/lvl_up/Defaultslider.vue";
-import ItemList from "src/components/lvl_up/ItemList.vue";
 import GlassElementHeading from "components/GlassElementHeading";
 
 export default defineComponent({
   name: "Research",
   components: {
-    InfoSlider,
+    DefenseSlider,
     DefaultSlider,
-    ItemList,
     GlassElementHeading,
   },
   setup() {
     const currentTab = ref("DefaultSlider");
     const researchInfo = ref();
 
-    function slideDiv(data) {
-      currentTab.value = "ResearchSlider";
-      researchInfo.value = data;
+    function slideDiv() {
+      currentTab.value = "DefenseSlider";
     }
 
     const currentTabComponent = computed(() => {
