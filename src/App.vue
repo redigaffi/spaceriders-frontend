00ff7ef95c622cdc169c540bcd95f231ac95f3fd
Muhammad Azhar <48376727+muazhar@users.noreply.github.com>
2022-01-20 12:10:45 -0800

    Landing Issues Resolved

--- src/App.vue
+++ src/App.vue
@@ -20,20 +20,19 @@ export default defineComponent({
   },
   methods: {
     update: async function (activePlanet) {
-      
       this.$store.commit("setActivePlanet", activePlanet);
-      
-      const tokenPrice = (await ApiRequest.tokenPrice());
+
+      const tokenPrice = await ApiRequest.tokenPrice();
       const allPlanetInfo = (await ApiRequest.getAllInfoPlanet(activePlanet.id))
         .data;
-      
-      this.$store.commit('setTokenPrice', {tokenPrice: tokenPrice});
-      this.$store.commit("setResourceData", allPlanetInfo.resources);      
+
+      this.$store.commit("setTokenPrice", { tokenPrice: tokenPrice });
+      this.$store.commit("setResourceData", allPlanetInfo.resources);
       this.$store.commit("setInstallationData", allPlanetInfo.installation);
       this.$store.commit("setResearchData", allPlanetInfo.research);
       this.$store.commit("setDefenseData", allPlanetInfo.defense);
       this.$store.commit("setConversionRequests", allPlanetInfo.conversion);
-      this.$store.commit("addEmails", {emails: allPlanetInfo.email});      
+      this.$store.commit("addEmails", { emails: allPlanetInfo.email });
     },
     updateAll: async function () {
       //TODO: Delete all intervals ..
@@ -50,7 +49,7 @@ export default defineComponent({
       this.$store.commit("setPlanets", planets);
 
       if (activePlanetId !== false) {
-        console.log("active planet")
+        console.log("active planet");
         let activePlanet = planets.filter((o) => o.id === activePlanetId);
         await this.update(activePlanet[0]);
 
@@ -58,7 +57,7 @@ export default defineComponent({
       } else if (!activePlanetId && planets.length > 0) {
         let activePlanets = planets.filter((o) => o.claimed);
         if (activePlanets.length > 0) {
-          console.log("no active planet")
+          console.log("no active planet");
           await this.update(activePlanets[0]);
         }
       }
