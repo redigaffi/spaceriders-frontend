8685d77c533e7321b9b09c5478e04cd9ca905ab0
José Ruiz <joseruiz@keemail.me>
2022-10-10 22:54:00 +0200

    SPR-736 Add partner to partners section

--- src/pages/Landing1.vue
+++ src/pages/Landing1.vue
@@ -762,7 +762,7 @@
     </section>
 
     <!-- PARTNERS -->
-    <!--
+
     <div id="partners"></div>
     <section class="custom-game">
       <div :class="$q.screen.lt.md ? 'q-px-md' : 'container'">
@@ -770,40 +770,25 @@
           <div class="text-2 title game__title">PARTNERS</div>
 
           <div class="q-pt-xl row flex-center items-center q-col-gutter-xl">
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <q-img src="https://images.ctfassets.net/xts27qnup0jr/373RVCYydODUjY5KqyKoTf/293e55e69fdbddbddd0d4af249e63653/cocacolalogo.png" alt="Coca-Cola">
+            <div class="partner-item col-xs-12 col-sm-6 col-md-3">
+              <a href="https://datacryptonft.com" target="_blank">
+                <q-img src="~assets/landing/img/partners/data.png" alt="Data">
                 </q-img>
+              </a>
             </div>
 
+            <!--
             <div class="col-xs-12 col-sm-6 col-md-3">
-                <q-img src="https://sabiogroup.com/sites/default/files/2021-08/computacenter_white.png" alt="Computacenter">
+              <a href="https://haechi.io/?lang=en" target="_blank">
+                <q-img src="~assets/landing/img/partners/haechi.png" alt="Haechi Labs">
                 </q-img>
+              </a>
             </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <q-img src="https://images.ctfassets.net/xts27qnup0jr/DIhbSZZ7PAPPt5QUUcJOi/25e44d06b71067cba9b63ee945f9be9f/vodafone-business-white.png" alt="Vodafone">
-                </q-img>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <q-img src="https://www.docker.com/wp-content/uploads/2022/03/Docker-Logo-White-RGB_Horizontal.png" alt="Docker">
-                </q-img>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <q-img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/625eb54bba1f7951f802c34c_Discord-Logo%2BWordmark-White (2).png" alt="Discord">
-                </q-img>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <q-img src="https://www.tailorbrands.com/wp-content/uploads/2020/03/Tailor-brands-White-transparent.png" alt="Tailor">
-                </q-img>
-            </div>
+            -->
           </div>
         </div>
       </div>
     </section>
-    -->
 
     <!-- TROUBLESHOOTING
     <div id="game"></div>
@@ -1431,7 +1416,7 @@ export default defineComponent({
   components: { Timeline },
 });
 </script>
-<style scoped>
+<style lang="scss" scoped>
 /* @import "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Orbitron:wght@400;500;600;700;900&display=swap"; */
 @import "~assets/landing/css/style.css";
 @import "~assets/landing/css/animate.min.css";
@@ -1452,6 +1437,14 @@ export default defineComponent({
 
 #overlay {
   position: relative;
+}
+
+.partner-item {
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 }
 
 #particles-js {
