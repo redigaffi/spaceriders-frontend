e08310ce8b5952aad58648871f315e0254722c0c
José Ruiz <joseruiz@keemail.me>
2022-11-21 04:23:32 +0100

    SPR-930 Improve plain email popup

--- src/components/email_templates/Plain.vue
+++ src/components/email_templates/Plain.vue
@@ -1,12 +1,33 @@
 <template>
-  {{ body }}
+  <q-separator />
+
+  <q-img :src="topicImage" style="height: 200px; width: 100%" />
+
+  <q-card-section>
+    {{ mail.body }}
+  </q-card-section>
 </template>
 <script setup>
-import { defineProps, reactive, toRefs } from "vue";
+import { defineProps, reactive, toRefs, computed } from "vue";
 
 const props = defineProps({
-  body: Object,
+  mail: Object,
 });
 
-const { body } = toRefs(props);
+const { mail } = toRefs(props);
+
+const topicImage = computed(() => {
+  let popupImage;
+
+  switch (mail.value.topic) {
+    case "level_up":
+      popupImage = "level_up_bg";
+      break;
+    default:
+      popupImage = "inbound_message_bg";
+      break;
+  }
+
+  return require(`../../assets/img/${popupImage}.jpg`);
+});
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -459,7 +459,7 @@
               </div>
             </q-card-section>
 
-            <component :is="templateName" :body="body"></component>
+            <component :is="templateName" :mail="body"></component>
           </q-card>
         </q-dialog>
       </q-page>
@@ -615,7 +615,7 @@ const body = computed(() => {
     case "space_pirates":
       return JSON.parse(activeEmail.value.body);
     case "plain":
-      return activeEmail.value.body;
+      return { body: activeEmail.value.body, topic: activeEmail.value.topic };
   }
 
   return "";
